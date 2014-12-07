93cf0be32a756262495fd609258ba812c655bdb9
David Wilkinson <david.wilkinson1984@gmail.com>
2014-12-07 13:37:53 +0000

    project 3 first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixelpic::Application.config.secret_token = 'aae797076b19c03c85c1306a6eed6c310e752f4f3c99882a243f1038ac6fc2d0d46efea766164b764d6139776cbb26cfaf58e0fd92c7221a1d0b4c77ef096ab6'
