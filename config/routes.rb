59c98c21be94239a2e93f5db80ad09b59fb75191
David Wilkinson <david.wilkinson1984@gmail.com>
2014-12-10 12:15:39 +0000

    added pages controller and root to: pages#index. added site nav. partialled off the navs etc from the layout into views/shared for easier  management

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Pixelpic::Application.routes.draw do
 
+  root to: "pages#index"
+  # match "/home" => "pages#index"
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "omniauth_callbacks", registrations: "users"}
 
   resources :images
   resources :mosaics
 
-  root to: "images#index"
 end
